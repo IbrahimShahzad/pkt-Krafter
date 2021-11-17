45a07eb785c0fe060bb8b526e523ec895864aa7b
Ibrahim Shahzad <ibrahim.shahzad@techavenue.biz>
2021-11-17 16:34:20 +0500

    udp layer

--- packet.py
+++ packet.py
@@ -16,29 +16,28 @@ class BaseClass:
         return '%(o1)s.%(o2)s.%(o3)s.%(o4)s' % locals()
     
     def mac_to_bytes(self,mac:str)->bytes:
-        delim = ':'
+        delimiter = ':'
         if len(mac) != 17:
             raise Exception('MAC Address Invalid Length')
-        if delim not in mac:
+        if delimiter not in mac:
             raise Exception('MAC Address should contain delimeter ":" ')
         return ("".join(mac.split(':'))).encode()
 
 class Ip(BaseClass):
-    def __init__(
-            self,
-            version:int = 4,
-            ttl_length:int =5,
-            type_of_service:int =252,
-            total_length:int =194,
-            identification:int =33683,
-            flags:int =0,
-            fragment_offset:int=0,
-            ttl:int=247,
-            protocol:int=17,
-            header_checksum:int =32147,
-            source_address:str = "10.65.139.49",
-            destination_address:str="10.180.32.226",
-            encoding:str='big') -> None:
+    def __init__(self,
+                version:int = 4,
+                ttl_length:int =5,
+                type_of_service:int =252,
+                total_length:int =194,
+                identification:int =33683,
+                flags:int =0,
+                fragment_offset:int=0,
+                ttl:int=247,
+                protocol:int=17,
+                header_checksum:int =32147,
+                source_address:str = "10.65.139.49",
+                destination_address:str="10.180.32.226",
+                encoding:str='big') -> None:
         self.verToS  = self.get_version_bytes(version=version,total_length=ttl_length)              # Version, IHL, Type of Service | Total Length
         self.type_of_service=type_of_service.to_bytes(1,encoding)  # Differentiated Services Field
         self.total_length=total_length.to_bytes(2,encoding)
@@ -50,12 +49,13 @@ class Ip(BaseClass):
         self.header_checksum = header_checksum.to_bytes(2,encoding)# b'\x7d\x93'  # Destination Address
         self.source_addr = super().ip_to_int(source_address).to_bytes(4,encoding)# b'\x0a\x41\x8b\x31'  # Destination Address
         self.destination_address = super().ip_to_int(destination_address).to_bytes(4,encoding) #b'\x0a\xb4\x20\xe2'  # Destination Address
+        print("init ip")
         return
-
     
     def create_ip_header(self)->bytes:
         """ Creates and returns an IP Header by appending all the IP layer attributes """
         self.ip_header=b''
+        print("creating ip header")
         attribute_names = [
             "verToS",
             "type_of_service",
@@ -81,28 +81,86 @@ class Ip(BaseClass):
 
 
 class Tcp:
-    pass
+    def __init__(self,
+                source_port=8000,
+                destination_port=9000,
+                sequence_number=23,
+                acknowledgement_number=2321,
+                data_offset=1,
+                reserved=1,
+                flags=1,
+                window_size=1,
+                checksum=12,
+                urgent_pointer=123,
+                encoding='big') -> None:
+        self.source_port=source_port.to_bytes(2,encoding)
+        self.destination_port=destination_port.to_bytes(2,encoding)
+        return
+
+class Udp:
+    def __init__(self,
+                source_port=9002,
+                destination_port=10040,
+                length=1240,
+                checksum=0,
+                encoding='big') -> None:
+        print("init udp")
+        self.source_port = source_port.to_bytes(2,encoding)
+        self.destination_port = destination_port.to_bytes(2,encoding)
+        self.length = length.to_bytes(2,encoding)
+        self.checksum = checksum.to_bytes(2,encoding)
+        pass
+
+    def create_udp_header(self)->bytes:
+        """ Creates and returns an IP Header by appending all the IP layer attributes """
+        print("creating udp header")
+        self.udp_header=b''
+        attribute_names = [
+            "source_port",
+            "destination_port",
+            "length",
+            "checksum"
+        ]
+        for attr in attribute_names:
+            self.udp_header+=self.__getattribute__(attr)
+        return self.udp_header
+
 
 class Ethernet(BaseClass):
-    def __init__(self,source_mac_address:str='00:00:5e:00:53:af',destination_mac_address:str='00:00:5e:00:53:af',ethernet_type:int=2048,encoding:str='big') -> None:
+    def __init__(self,
+                source_mac_address:str='00:00:5e:00:53:af',
+                destination_mac_address:str='00:00:5e:00:53:af',
+                ethernet_type:int=2048,
+                encoding:str='big') -> None:
         self.source = super().mac_to_bytes(source_mac_address)              # b'\x30\x29\x52\x36\x7b\x2c'
         self.destination = super().mac_to_bytes(destination_mac_address)    # b'\x00\x0c\x29\x88\xe5\x81'
         self.type = ethernet_type.to_bytes(2,encoding)                      # b'\x08\x00'
         return
 
-class udp_header:
-    def __init__(self) -> None:
-        return
 
-class Packet(Ip):
+class Packet(Udp,Ip):
     def __init__(self) -> None:
-        super().__init__()
+        Udp.__init__(self)
+        Ip.__init__(self)
+        #super(Ip).__init__()
 
+    def create_udp_packet(self)->bytes:
+        udp = super().create_udp_header()
+        ip = super().create_ip_header()
+        return udp + ip
 
 def func():
-    s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_TCP)
+    sample_packet=Packet()
+    print(dir(sample_packet))
+    print(sample_packet.create_udp_packet())
+    s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_UDP)
+    #s = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_TCP)
     s.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
+    #packet = ip_header + tcp_header
+    s.sendto(sample_packet, ('10.10.10.1', 0))
 
+if __name__=="__main__":
+    func()
 
 #s=Packet()
 #s.create_ip_header()
